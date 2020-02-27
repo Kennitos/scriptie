b15e5a6ce4b61e048c4f60697c7890490a31cb80
Kennet <you@example.com>
2020-02-27 14:45:31 +0100

    move files

--- SE1.py
+++
@@ -1,8 +0,0 @@
-# import
-import json
-import os
-from collections import defaultdict
-import numpy as np
-
-folder = 'PythonDataScienceHandbook-notebooks'
-files = [file for file in os.listdir(os.getcwd()+'\\'+folder) if file[-6:]=='.ipynb']
