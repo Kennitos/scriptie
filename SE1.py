60d0ca73ba93b396d54766a9c46e769d9f396f29
Kennet <you@example.com>
2020-02-27 13:58:09 +0100

    test update

--- SE1.py
+++ SE1.py
@@ -3,3 +3,6 @@ import json
 import os
 from collections import defaultdict
 import numpy as np
+
+folder = 'PythonDataScienceHandbook-notebooks'
+files = [file for file in os.listdir(os.getcwd()+'\\'+folder) if file[-6:]=='.ipynb']
